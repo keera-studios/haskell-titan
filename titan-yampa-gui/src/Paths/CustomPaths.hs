b5ec0d17c90bc6f888e54fb6b159d97392809c64
Ivan Perez <ivan.perez@keera.co.uk>
2017-01-22 21:31:01 +0200

    Fixes module name in import

--- titan-yampa-gui/src/Paths/CustomPaths.hs
+++ titan-yampa-gui/src/Paths/CustomPaths.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE CPP #-}
 module Paths.CustomPaths
-  (module Paths_yampa_debugging_gui
+  (module Paths_titan_yampa_gui
 #ifndef linux_HOST_OS
   , module Paths.CustomPaths
 #endif
   )
  where
 
-import Paths_yampa_debugging_gui
+import Paths_titan_yampa_gui
 
 #ifndef linux_HOST_OS
 vendorKey :: String
