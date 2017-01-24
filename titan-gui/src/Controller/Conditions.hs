a5e0f903a3e14eba82ebedca41bec1b2853cd006
Ivan Perez <ivan.perez@keera.co.uk>
2017-01-24 10:14:08 +0200

    Close window cleanly

--- titan-gui/src/Controller/Conditions.hs
+++ titan-gui/src/Controller/Conditions.hs
@@ -1,12 +1,14 @@
 module Controller.Conditions where
 
 import CombinedEnvironment
-import Controller.Conditions.BtnStep as BtnStep
+import Controller.Conditions.BtnStep      as BtnStep
 import Controller.Conditions.CurFrameEdit as CurFrameEdit
-import Controller.Conditions.TraceViewer as TraceViewer
+import Controller.Conditions.TraceViewer  as TraceViewer
+import Controller.Conditions.CloseIDE     as CloseIDE
 
 installHandlers :: CEnv -> IO ()
 installHandlers cenv = do
   BtnStep.installCondition      cenv
   CurFrameEdit.installCondition cenv
   TraceViewer.installCondition  cenv
+  CloseIDE.installCondition     cenv
---
+++ titan-gui/src/Controller/Conditions/CloseIDE.hs
@@ -0,0 +1,66 @@
+-- | Condition: The program ends when the main window is closed
+
+module Controller.Conditions.CloseIDE
+    (installCondition)
+  where
+
+-- External libraries
+import Control.Arrow
+import Control.Monad
+import Control.Monad.Reader (liftIO)
+import Graphics.UI.Gtk
+
+-- Internal libraries
+import CombinedEnvironment hiding (installCondition)
+import View.Objects
+
+-- TODO:
+-- filter :: (a -> Bool) -> RV a -> RV a
+-- only changes when f a == True
+--
+-- edge :: (a -> Bool) -> RV a -> RV a
+-- only changes when f a becomes True after being False
+--
+-- also filterM and edgeM
+--
+-- liftIO :: m a -> RO a m
+--
+-- wrapIO :: m a -> (a -> m ()) -> RV a m -- Passive
+--
+-- The following probably exists already:
+-- wrapWO :: (a -> m b) -> RV a
+--
+
+installCondition :: CEnv -> IO()
+installCondition cenv = void $ do
+  mw <- mainWindow $ uiBuilder $ view cenv
+  mw `on` deleteEvent $ liftIO $ conditionVM cenv
+
+-- | Enforces the condition in View to Model direction
+conditionVM :: CEnv -> IO Bool
+conditionVM cenv = do
+  b <- checkExit cenv
+  when b $ onViewAsync destroyView
+  return (not b)
+
+-- Returns true if the operation can continue, false otherwise
+checkExit :: CEnv -> IO Bool
+checkExit cenv = do
+  let (v,m) = (view &&& model) cenv
+  let ui = uiBuilder v
+  win <- mainWindow ui
+  dialog <- messageDialogNew (Just win) [DialogModal] MessageQuestion ButtonsNone
+              "Exit?"
+  dialogAddButton dialog "Cancel"               ResponseCancel
+  dialogAddButton dialog "Exit"                 ResponseYes
+
+  dialogSetDefaultResponse dialog ResponseCancel
+
+  -- Run the dialog and process the result
+  widgetShowAll dialog
+  r <- dialogRun dialog
+  res <- case r of
+           ResponseYes -> return True
+           _           -> return False -- Cancel or close the dialog without answering
+  widgetDestroy dialog
+  return res
