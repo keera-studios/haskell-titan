8e4509cb13e3cf9638d81b288b45fcb780f7ba24
Ivan Perez <ivan.perez@keera.co.uk>
2017-02-01 14:53:45 +0100

    Lint

--- titan-gui/src/Controller/Conditions.hs
+++ titan-gui/src/Controller/Conditions.hs
@@ -2,13 +2,13 @@ module Controller.Conditions where
 
 import CombinedEnvironment
 import Controller.Conditions.Buttons      as Buttons
+import Controller.Conditions.CloseIDE     as CloseIDE
 import Controller.Conditions.CurFrameInfo as CurFrameInfo
 import Controller.Conditions.TraceViewer  as TraceViewer
-import Controller.Conditions.CloseIDE     as CloseIDE
 
 installHandlers :: CEnv -> IO ()
 installHandlers cenv = do
   Buttons.installCondition      cenv
+  CloseIDE.installCondition     cenv
   CurFrameInfo.installCondition cenv
   TraceViewer.installCondition  cenv
-  CloseIDE.installCondition     cenv
